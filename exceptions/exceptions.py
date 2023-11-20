fee5832b90aa1c839a51880615b1b70d90ae79b4
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2023-11-20 00:14:13 -0600

    fix: excepciones innecesarias quitadas, excepcion TokenIncorrecto ahora muestra la posicion del error

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,13 +1,3 @@
-class EstadoNoExistente(Exception):
-    def __init__(self):
-        self.message = "El estado no pertenece al conjunto de estados del automata!"
-        super().__init__(self.message)
-
-class SimboloNoExistente(Exception):
-    def __init__(self):
-        self.message = "El caracter no pertenece al alfabeto del automata!"
-        super().__init__(self.message)
-
 class ArchivoVacio(Exception):
     def __init__(self):
         self.message = "el archivo esta vacio!"
@@ -19,6 +9,6 @@ class EstadoSiguienteNoExiste(Exception):
         super().__init__(self.message)
 
 class TokenIncorrecto(Exception):
-    def __init__(self):
-        self.message = "Token incorrecto."
+    def __init__(self, cordinates: tuple[str, str]):
+        self.message = f"Token incorrecto en fila {cordinates[0]}, columna {cordinates[1]}"
         super().__init__(self.message)
