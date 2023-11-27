802b429ec5452e314d8e1c22534186addb9dee48
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2023-11-27 06:03:10 -0600

    coordenadas iniciales agregadas

--- dfa/token.py
+++ dfa/token.py
@@ -4,8 +4,11 @@ class Token:
     def __init__(self, type: str, lexem : str, coordinates: tuple[int, int]):
         self.type : str = type
         self.lexem : str = lexem
-        self.coordinates : tuple[int, int] = coordinates
+        self.final_coordinates : tuple[int, int] = coordinates
+        inicio, fin = coordinates
+
+        self.initial_coordinates : tuple[int, int] = (inicio, fin - len(lexem))
 
 
     def __str__(self) -> str:
-        return f"<TYPE:{self.type},  LEXEM: {self.lexem},  POSITION: {self.coordinates}>"
+        return f"<TYPE:{self.type},  LEXEM: {self.lexem}, INITIAL POSITION: {self.initial_coordinates}, FINAL POSITION: {self.final_coordinates} >"
