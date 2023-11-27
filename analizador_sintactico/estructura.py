3ac7ac44b4ed94aa6afe961319d77c4994bdd00e
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2023-11-26 22:32:18 -0600

    clase necesaria para almacenar la informacion de la posible estructura

---
+++ analizador_sintactico/estructura.py
@@ -0,0 +1,11 @@
+class Structure:
+    def __init__(self):
+        self.nombre_estructura : str = None
+        self.nombre_campo : str = None
+        self.tipo_campo = str = None
+        self.valor_campo : str = None
+        self.nombre_apuntador : str = None
+        self.valor_apuntador : str = None
+        self.tipo_apuntador : str = None
+        pass
+    
